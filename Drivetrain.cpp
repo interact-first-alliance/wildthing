4b8bedb16082ca30425385aa662643c3a62c29c4
Andrew-Cua <andrew@cuafamily.com>
2019-06-20 12:22:10 -0400

    Revert "refractored DualJoystick and Drivetrain so that the Drivetrain class isnt doing modifications that the DualJoystick class should be handling"
    This reverts commit 997493144163e3ff5a28584f0df49845dd835d1d.

--- Drivetrain.cpp
+++ Drivetrain.cpp
@@ -17,12 +17,16 @@ void Drivetrain::setPower(int power,Servo* motor)
   motor->writeMicroseconds(power);
 }
 
-void Drivetrain::cartesianDrive(double rotPow, double movePow)
+void Drivetrain::cartesianDrive(int x, int y)
 {
   //reads the potentiometer and scales to from 1 to 0
-  double potentiometerReading = analogRead(m_potentiometerPin);
-  double powerScale = mapf(potentiometerReading, 0, 1023, 1,0);
-  //multiplies the combined joystick values to the power scale
+  float potentiometerReading = analogRead(m_potentiometerPin);
+  float powerScale = mapf(potentiometerReading, 0, 1023, 1,0);
+  //scales the x and y from the joystick to -1 - 1 similar to FRC so you can
+  //combine the values given from the x and y values given by the joystick
+  float rotPow = mapf(x,0,1023,-1,1);
+  float movePow  = mapf(y,0,1023,1,-1);
+  //multiplies the scaled joystick values to the power scale
   float leftPow = constrain((movePow - steerPow), -1,1) * powerScale;
   float rightPow= constrain((movePow + steerPow), -1,1) * powerScale;
   //remaps the scales x and y back to servo values so it can drive motors
